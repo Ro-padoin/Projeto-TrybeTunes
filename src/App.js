0b0d7a0d2eb3812bae8d2edfeff8565728e68f8f
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-01-13 15:03:28 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+// React-Router:
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Components:
+import Album from './component/Album';
+import Favorites from './component/Favorites';
+import Login from './component/Login';
+import NotFound from './component/NotFound';
+import Profile from './component/Profile';
+import ProfileEdit from './component/ProfileEdit';
+import Search from './component/Search';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTune</p>);
+    return (
+      <section>
+        <BrowserRouter>
+          <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </BrowserRouter>
+      </section>
+    );
   }
 }
 
