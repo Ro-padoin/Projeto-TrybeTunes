3d6211400b5bc2421e262a93f1ed3b0dc853725b
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-03-31 07:40:28 -0300

    Loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Loading extends Component {
+class Loading extends React.Component {
   render() {
     return (
       <div>
